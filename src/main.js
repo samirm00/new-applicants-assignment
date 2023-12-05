01d71a5c46442fedfcf4eb9b0661ece0d7f8dacb
samirm00 <sammoham.be@gmail.com>
2023-12-05 22:11:04 +0100

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,35 @@
-// Add your JavaScript here
+// DOM elements
+const buttonsContainer = document.querySelector('.buttons');
+const screen = document.querySelector('.display');
+
+// util
+const calculateResult = (expr) => {
+    try {
+        return Function('return ' + expr)();
+    } catch (error) {
+        return 'Error';
+    }
+};
+
+// handler
+const buttonClickHandler = (e) => {
+    const btnValue = e.target.textContent;
+
+    switch (btnValue) {
+        case '=':
+            screen.value = calculateResult(screen.value);
+            break;
+        case 'AC':
+            screen.value = '0';
+            break;
+        case 'DEL':
+            screen.value = screen.value.slice(0, -1);
+            break;
+        default:
+            screen.value += btnValue;
+            break;
+    }
+};
+
+// event
+buttonsContainer.addEventListener('click', buttonClickHandler);
